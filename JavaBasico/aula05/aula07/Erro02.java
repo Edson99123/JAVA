a9a4b9588ae3ae2e37fe0335e660350be828a9fe
Edson99123 <edsonsales99@gmail.com>
2024-02-01 17:16:43 -0400

    Apredendo sobre erros no java, forçando erros com falta de virgula primeiro for

---
+++ JavaBasico/aula05/aula07/Erro02.java
@@ -0,0 +1,13 @@
+/***
+ * Autor: Edson Souza
+ * Data: 01 02 2024
+ */
+
+package aula07;
+public class Erro02 {
+    public static void main(String[] args){
+        for (int i = 10; i >= 1; i--){
+            System.out.println("Teste variavel! " + i);
+        }
+    }
+}
